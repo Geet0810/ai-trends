d1220bac1d4607adea8c781a53601f1154cf1b72
Geet0810 <geetanjalidwivedi08@gmail.com>
2026-01-20 19:58:47 +0530

    Update data loading paths to use BASE_DIR

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,8 +1,12 @@
+import os
 import pandas as pd
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 def load_data():
     return {
-        "jobs": pd.read_csv("data/ai_jobs.csv"),
-        "skills": pd.read_csv("data/skills_demand.csv"),
-        "country": pd.read_csv("data/country_ai_trends.csv")
+        "jobs": pd.read_csv(os.path.join(BASE_DIR, "data/ai_jobs.csv")),
+        "skills": pd.read_csv(os.path.join(BASE_DIR, "data/skills_demand.csv")),
+        "country": pd.read_csv(os.path.join(BASE_DIR, "data/country_ai_trends.csv"))
     }
+
